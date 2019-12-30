b5f588a17f2a530c026fcad2ca58c9b58a0c88c9
Harry Venables <harry.venables@immersivelabs.co.uk>
2019-12-30 17:01:57 +0000

    heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :platform, PlatformWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [schema: "https", host: "harry-platform.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
