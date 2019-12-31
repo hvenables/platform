d219d72a5e9664638bdf6d14c8277761357db761
Harry Venables <harry.venables@immersivelabs.co.uk>
2019-12-31 21:14:32 +0000

    Set up elm architecture

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,8 +1,46 @@
 module Main exposing (main)
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+
+-- MAIN
 
 
 main : Html msg
 main =
-    text "Hello from Elm inside Pheonix!"
+    div []
+        [ h1 [] [ text "Games" ]
+        , gamesIndex model
+        ]
+
+
+
+-- MODEL
+
+
+model : List String
+model =
+    [ "Platform Game"
+    , "Aventure Game"
+    ]
+
+
+
+-- VIEW
+
+
+gamesIndex : List String -> Html msg
+gamesIndex gameTitles =
+    div [ class "games-index" ] [ gamesList gameTitles ]
+
+
+gamesList : List String -> Html msg
+gamesList gameTitles =
+    ul [ class "games-list" ] (List.map gamesListItem gameTitles)
+
+
+gamesListItem : String -> Html msg
+gamesListItem gameTitle =
+    li [] [ text gameTitle ]
